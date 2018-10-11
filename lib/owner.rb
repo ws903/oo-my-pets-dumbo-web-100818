2206c5d0ab3a473f33bada81a924afbc4147e344
ws903 <ws903@nyu.edu>
2018-10-11 17:00:34 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,67 @@
 class Owner
-  # code goes here
+	attr_accessor(:name, :pets)
+	attr_reader(:species)
+
+	@@all = []
+
+	def initialize(species)
+		@species = species
+		@pets = {:fishes => [], :dogs => [], :cats => []}
+		@@all.push(self)
+	end
+
+	def say_species
+		"I am a #{@species}."
+	end
+
+	def buy_fish(name)
+		fish = Fish.new(name)
+		@pets[:fishes].push(fish)
+	end
+
+	def feed_fish
+		@pets[:fishes].map {|fish| fish.mood = "happy"}
+	end
+
+	def buy_cat(name)
+		cat = Cat.new(name)
+		@pets[:cats].push(cat)
+	end
+
+	def play_with_cats
+		@pets[:cats].map {|cat| cat.mood = "happy"}
+	end
+
+	def buy_dog(name)
+		dog = Dog.new(name)
+		@pets[:dogs].push(dog)
+	end
+
+	def walk_dogs
+		@pets[:dogs].map {|dog| dog.mood = "happy"}
+	end
+
+	def list_pets
+		pets_str = "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+	end
+
+	def sell_pets
+		@pets.keys.each {|pet_kind|
+			@pets[pet_kind].map {|pet| pet.mood = "nervous"}
+		}
+		@pets = {:fishes => [], :dogs => [], :cats => []}
+	end
+
+	def self.count
+		@@all.count
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.reset_all
+		@@all.clear
+	end
+
 end
